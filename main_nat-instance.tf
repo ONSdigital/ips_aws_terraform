9450123ced363df101f818f85dbe40fc9cb81c2b
Ashutosh Jindal <Ash.Jindal@ext.ons.gov.uk>
2020-05-05 10:51:51 +0100

    Do not stop the SSHD service since the NAT serves as Bastion too
    Task: LU-6260

--- main_nat-instance.tf
+++ main_nat-instance.tf
@@ -100,7 +100,6 @@ resource "aws_instance" "nat" {
 #!/bin/bash
 sudo sysctl -w net.ipv4.ip_forward=1
 sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-sudo service sshd stop
 sudo systemctl stop rpcbind
   EOF
 
